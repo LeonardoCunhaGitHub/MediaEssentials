285b490e452185b899811af92047a7663a19003a
US-LCUNHA\Leonardo Cunha <leonardo.cunha@valtech.com>
2019-08-19 14:37:19 -0400

    3 features pages

---
+++ Common/ItemsPublishingTask.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Publishing;
+using Sitecore.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediaEssentials.Common
+{
+    public class ItemsPublishingTask
+    {
+        public void Execute(Item[] itemArray, CommandItem commandItem, ScheduleItem scheduleItem)
+        {
+            foreach (Item item in itemArray)
+            {
+                PublishItem(item);
+            }
+        }
+
+
+        public void PublishItem(Item item)
+        {
+            try
+            {
+                PublishOptions p = new PublishOptions(Sitecore.Data.Database.GetDatabase("master"), Sitecore.Data.Database.GetDatabase("web"), PublishMode.SingleItem, Sitecore.Context.Language, DateTime.Now);
+                p.RootItem = item;
+                p.Deep = true; // Publishing subitems
+
+                (new Publisher(p)).Publish();
+            }
+            catch (Exception e)
+            {
+                Sitecore.Diagnostics.Log.Error("Exception, PublishItem: " + e.Message, this);
+            }
+        }
+    }
+}
---
+++ MediaSize.aspx.cs
@@ -0,0 +1,40 @@
+﻿using MediaEssentials.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MediaEssentials
+{
+    public partial class MediaSize : System.Web.UI.Page
+    {
+
+        private readonly MediaLibraryUtils _mediaLibrary = new MediaLibraryUtils();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+
+            _mediaLibrary.SetDatabaseDropDown(ddDataBase);
+
+            _mediaLibrary.SetMediaFoldersDropDown(ddMediaFolders, ddDataBase);
+        }
+
+        protected void ddDataBase_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            _mediaLibrary.SetMediaFoldersDropDown(ddMediaFolders, ddDataBase);
+        }
+
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
